d7a8999337c5661920b22dda07105a2b3dd1bca8
Jian Yeung <yeungjian@gmail.com>
2019-07-17 14:21:44 -0700

    finished es6 stack

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,25 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  size() {
+    return Object.keys(this.storage).length;
+  }
+
+  enqueue(val) {
+    var keys = Object.keys(this.storage);
+    var key = keys[key.length - 1] + 1;
+    this.storage[key] = val;
+    return val;
+  }
+
+  dequeue() {
+    var keys = Object.keys(this.storage);
+    var key = keys[0];
+    var val = this.storage[key];
+    delete this.storage[key];
+    return val;
+  }
 }
