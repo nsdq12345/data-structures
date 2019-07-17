f4db6822f43b4882eed3dd585fbe25ac3f65c189
Jian Yeung <yeungjian@gmail.com>
2019-07-17 11:45:25 -0700

    finished basic enqueue dequeue size for queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,23 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var keys = Object.keys(storage);
+    var key = keys[keys.length-1] + 1;
+    storage[''+key] = value;
+    return value;
   };
 
   someInstance.dequeue = function() {
+    var keys = Object.keys(storage);
+    var key = keys[0];
+    var value = storage[key];
+    delete storage[key];
+    return value;
   };
 
   someInstance.size = function() {
+    var keys = Object.keys(storage);
+    return keys.length;
   };
 
   return someInstance;
