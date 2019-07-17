31dd82cb545cb38ffa8c81a90a04f09b2ff7c0d8
Jian Yeung <yeungjian@gmail.com>
2019-07-17 13:22:55 -0700

    finished pseudoclassical queue

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage  = {};
+};
+
+Stack.prototype.size = function () {
+  return Object.keys(this.storage).length;
+};
+
+Stack.prototype.push = function (val) { 
+  var keys = Object.keys(this.storage);
+  var key = keys.length;
+  this.storage[key] = val;
+  return val;
+};
+
+Stack.prototype.pop = function () {
+  var keys = Object.keys(this.storage);
+  var key = keys.length - 1;
+  var val = this.storage[key];
+  delete this.storage[key];
+  return val;
 };
 
 
