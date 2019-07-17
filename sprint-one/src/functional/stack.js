c8c68d2a2fb0da91af12636c93163ed9e9f96ee5
Jian Yeung <yeungjian@gmail.com>
2019-07-17 11:40:26 -0700

    finished basic push pop size

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,24 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var keys = Object.keys(storage);
+    var key = keys.length;
+    storage['' + key] = value;
+    return value;
   };
 
   someInstance.pop = function() {
+    var keys = Object.keys(storage);
+    var key = keys.length-1;
+    var poppedValue = storage[''+key];
+    console.log("TryingToDelete:" + storage[''+key] + " Key:" + key);
+    delete storage[''+key];
+    return poppedValue;
   };
 
   someInstance.size = function() {
+    var keys = Object.keys(storage);
+    return keys.length;
   };
 
   return someInstance;
