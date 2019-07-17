5085de3f45fcfab03d4f66cf34ba843472fb4fb7
Jian Yeung <yeungjian@gmail.com>
2019-07-17 12:26:48 -0700

    finished function stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.storage = {};
+
+  _.extend(stack, stackMethods);
+  return stack;
 };
 
 var stackMethods = {};
 
 
+stackMethods.push = function (val) {
+  var keys = Object.keys(this.storage);
+  var key = keys.length;
+  this.storage[key] = val;
+  return val;
+};
+
+stackMethods.pop = function () {
+  var keys = Object.keys(this.storage);
+  var key = keys.length - 1;
+  var value = this.storage[key];
+  delete this.storage[key];
+  return value;
+};
+
+stackMethods.size = function () {
+  var keys = Object.keys(this.storage);
+  return keys.length;
+};
