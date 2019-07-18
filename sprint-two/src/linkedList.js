02a11718d14998a4cb45fa2ca9b8d5861969925d
nsdq12345 <yeungjian@gmail.com>
2019-07-17 20:41:20 -0700

    Finished linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,40 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.tail == null) {
+      var node = Node(value);
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = Node(value);
+      list.tail = Node(value);
+    }
   };
 
   list.removeHead = function() {
+    var oldHead = list.head;
+    list.head = list.head.next;
+    return oldHead.value;
   };
 
   list.contains = function(target) {
+    if (list.head.value === target) {
+      return true;
+    } else if (list.head.next != null) {
+      return nodeContains(list.head.next);
+    } else {
+      return false;
+    }
+
+    function nodeContains(node) {
+      if (node.value === target) {
+        return true;
+      } else if (node.next != null){
+        nodeContains(node.next);
+      } else {
+        return false;
+      }
+    }
   };
 
   return list;
